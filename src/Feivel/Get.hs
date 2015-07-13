610c777d238663d6b1869572ef38da35c1f5ab73
nathan <nbloomf@gmail.com>
2015-07-13 10:35:54 -0500

    Refactor in Eval: move Typed and Get classes to their own files

---
+++ src/Feivel/Get.hs
@@ -0,0 +1,267 @@
+{---------------------------------------------------------------------}
+{- Copyright 2015 Nathan Bloomfield                                  -}
+{-                                                                   -}
+{- This file is part of Feivel.                                      -}
+{-                                                                   -}
+{- Feivel is free software: you can redistribute it and/or modify    -}
+{- it under the terms of the GNU General Public License version 3,   -}
+{- as published by the Free Software Foundation.                     -}
+{-                                                                   -}
+{- Feivel is distributed in the hope that it will be useful, but     -}
+{- WITHOUT ANY WARRANTY; without even the implied warranty of        -}
+{- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the      -}
+{- GNU General Public License for more details.                      -}
+{-                                                                   -}
+{- You should have received a copy of the GNU General Public License -}
+{- along with Feivel. If not, see <http://www.gnu.org/licenses/>.    -}
+{---------------------------------------------------------------------}
+
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+module Feivel.Get (Get, get, getVal) where
+
+import Feivel.Type
+import Feivel.Error
+import Feivel.Typed
+import Feivel.Locus
+import Feivel.EvalM
+import Feivel.Expr
+import Feivel.Lib
+import Feivel.Store
+
+class Get a where
+  get :: Expr -> EvalM a
+
+getVal :: (ToExpr a, Get b) => a -> EvalM b
+getVal x = get (toExpr x)
+
+instance Get Expr where
+  get expr = return expr
+
+
+{----------------}
+{- :Get:IntExpr -}
+{----------------}
+
+instance Get IntExpr where
+  get (IntE y) = return y
+  get v = do
+    t <- typeOf v
+    reportErr (locusOf v) $ TypeMismatch ZZ t
+
+instance Get Integer where
+  get expr = do
+    x <- get expr :: EvalM IntExpr
+    case x of
+      IntConst k :@ _ -> return k
+      v -> reportErr (locusOf v) UnevaluatedExpression
+
+
+{----------------}
+{- :Get:StrExpr -}
+{----------------}
+
+instance Get StrExpr where
+  get (StrE y) = return y
+  get (DocE y) = do  -- this shouldn't be necessary. why is it?
+    str <- toGlyph y
+    return $ StrConst str :@ (locusOf y)
+  get v = do
+    t <- typeOf v
+    reportErr (locusOf v) $ TypeMismatch SS t
+
+instance Get String where
+  get expr = do
+    x <- get expr
+    case x of
+      StrConst s :@ _ -> return s
+      v -> reportErr (locusOf v) UnevaluatedExpression
+
+
+{-----------------}
+{- :Get:BoolExpr -}
+{-----------------}
+
+instance Get BoolExpr where
+  get (BoolE y) = return y
+  get v = do
+    t <- typeOf v
+    reportErr (locusOf v) $ TypeMismatch BB t
+
+instance Get Bool where
+  get expr = do
+    x <- get expr
+    case x of
+      BoolConst b :@ _ -> return b
+      v -> reportErr (locusOf v) UnevaluatedExpression
+
+
+{----------------}
+{- :Get:RatExpr -}
+{----------------}
+
+instance Get RatExpr where
+  get (RatE y) = return y
+  get v = do
+    t <- typeOf v
+    reportErr (locusOf v) $ TypeMismatch QQ t
+
+instance Get Rat where
+  get expr = do
+    x <- get expr
+    case x of
+      RatConst r :@ _ -> return r
+      v -> reportErr (locusOf v) UnevaluatedExpression
+
+
+{-----------------}
+{- :Get:ListExpr -}
+{-----------------}
+
+instance Get ListExpr where
+  get (ListE y) = return y
+  get v = do
+    t <- typeOf v
+    reportErr (locusOf v) $ ListExpected t
+
+instance (Get a) => Get [a] where
+  get expr = do
+    x <- getList expr
+    sequence $ fmap get x
+    where
+      getList :: Expr -> EvalM [Expr]
+      getList w = do
+        case w of
+          ListE (ListConst _ xs :@ _) -> return xs
+          ListE v -> reportErr (locusOf v) UnevaluatedExpression
+          v -> do
+            t <- typeOf v
+            reportErr (locusOf v) $ ListExpected t
+
+
+{----------------}
+{- :Get:MacExpr -}
+{----------------}
+
+instance Get MacExpr where
+  get (MacE y) = return y
+  get v = do
+    t <- typeOf v
+    reportErr (locusOf v) $ MacroExpected t
+
+instance Get (Store Expr, Expr) where
+  get expr = do
+    case expr of
+      MacE (MacConst _ vals y (amb,_) :@ loc) -> do
+        st <- toStateT loc vals
+        return (mergeState st amb, y)
+      MacE v -> reportErr (locusOf v) UnevaluatedExpression
+      v -> do
+        t <- typeOf v
+        reportErr (locusOf v) $ MacroExpected t
+
+
+{----------------}
+{- :Get:MatExpr -}
+{----------------}
+
+instance Get MatExpr where
+  get (MatE m) = return m
+  get v = do
+    t <- typeOf v
+    reportErr (locusOf v) $ MatrixExpected t
+
+instance (Get a) => Get (Matrix a) where
+  get expr = do
+    x <- getMatrix expr
+    mSeq $ fmap get x
+    where
+      getMatrix :: Expr -> EvalM (Matrix Expr)
+      getMatrix w = do
+        case w of
+          MatE (MatConst _ m :@ _) -> return m
+          MatE v -> reportErr (locusOf v) UnevaluatedExpression
+          v -> do
+            t <- typeOf v
+            reportErr (locusOf v) $ MatrixExpected t
+
+
+{------------}
+{- :Get:Doc -}
+{------------}
+
+instance Get Doc where
+  get (DocE y) = return y
+  get v = do
+    t <- typeOf v
+    reportErr (locusOf v) $ TypeMismatch DD t
+
+
+{-----------------}
+{- :Get:PolyExpr -}
+{-----------------}
+
+instance Get PolyExpr where
+  get (PolyE m) = return m
+  get v = do
+    t <- typeOf v
+    reportErr (locusOf v) $ PolynomialExpected t
+
+instance (Get a) => Get (Poly a) where
+  get expr = do
+    x <- getPoly expr
+    polySeq $ fmap get x
+    where
+      getPoly :: Expr -> EvalM (Poly Expr)
+      getPoly w = do
+        case w of
+          PolyE (PolyConst _ m :@ _) -> return m
+          PolyE v -> reportErr (locusOf v) UnevaluatedExpression
+          v -> do
+            t <- typeOf v
+            reportErr (locusOf v) $ PolynomialExpected t
+
+
+{-----------------}
+{- :Get:PermExpr -}
+{-----------------}
+
+instance Get PermExpr where
+  get (PermE m) = return m
+  get v = do
+    t <- typeOf v
+    reportErr (locusOf v) $ PermutationExpected t
+
+instance Get (Perm Expr) where
+  get expr = do
+    case expr of
+      PermE (PermConst _ m :@ _) -> return m
+      PermE v -> reportErr (locusOf v) UnevaluatedExpression
+      v -> do
+        t <- typeOf v
+        reportErr (locusOf v) $ PermutationExpected t
+
+instance Get (Perm Integer) where
+  get expr = do
+    x <- get expr :: EvalM (Perm Expr)
+    seqPerm $ mapPerm get x
+
+
+{------------------}
+{- :Get:ZZModExpr -}
+{------------------}
+
+instance Get ZZModExpr where
+  get (ZZModE y) = return y
+  get v = do
+        t <- typeOf v
+        reportErr (locusOf v) $ ModularIntegerExpected t
+
+instance Get ZZModulo where
+  get expr = do
+    x <- get expr :: EvalM ZZModExpr
+    case x of
+      ZZModConst k :@ _ -> return k
+      v -> reportErr (locusOf v) UnevaluatedExpression
+
---
+++ src/Feivel/Typed.hs
@@ -0,0 +1,466 @@
+{---------------------------------------------------------------------}
+{- Copyright 2015 Nathan Bloomfield                                  -}
+{-                                                                   -}
+{- This file is part of Feivel.                                      -}
+{-                                                                   -}
+{- Feivel is free software: you can redistribute it and/or modify    -}
+{- it under the terms of the GNU General Public License version 3,   -}
+{- as published by the Free Software Foundation.                     -}
+{-                                                                   -}
+{- Feivel is distributed in the hope that it will be useful, but     -}
+{- WITHOUT ANY WARRANTY; without even the implied warranty of        -}
+{- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the      -}
+{- GNU General Public License for more details.                      -}
+{-                                                                   -}
+{- You should have received a copy of the GNU General Public License -}
+{- along with Feivel. If not, see <http://www.gnu.org/licenses/>.    -}
+{---------------------------------------------------------------------}
+
+{-# LANGUAGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances #-}
+
+module Feivel.Typed where
+
+import Feivel.Type
+import Feivel.Expr
+import Feivel.Locus
+import Feivel.Error
+import Feivel.EvalM
+import Feivel.Lib
+
+{---------------}
+{- :Instances  -}
+{-   :ListExpr -}
+{-   :MacExpr  -}
+{-   :MatExpr  -}
+{---------------}
+
+
+{----------}
+{- :Typed -}
+{----------}
+
+class Typed t where
+  typeOf :: t -> EvalM Type
+
+
+
+unifyTypesOf :: (Typed a, Typed b) => Locus -> a -> b -> EvalM Type
+unifyTypesOf loc a b = do
+  ta <- typeOf a
+  tb <- typeOf b
+  case unify ta tb of
+    Left err -> reportErr loc err
+    Right t -> return t
+
+expectType :: (Typed a) => Locus -> Type -> a -> EvalM Type
+expectType loc t x = do
+  u <- typeOf x
+  if t == u
+    then return u
+    else reportErr loc $ TypeMismatch t u
+
+expectMatrix :: (Typed a) => Locus -> a -> EvalM Type
+expectMatrix loc x = do
+  u <- typeOf x
+  case u of
+    MatOf t -> return t
+    _ -> reportErr loc $ MatrixExpected u
+
+sameType :: (Typed a, Typed b) => Locus -> a -> b -> EvalM Type
+sameType loc a b = do
+  ta <- typeOf a
+  tb <- typeOf b
+  if ta == tb
+    then return ta
+    else reportErr loc $ TypeMismatch ta tb
+
+
+{--------------}
+{- :Instances -}
+{--------------}
+
+instance Typed IntExpr  where typeOf _ = return ZZ
+instance Typed StrExpr  where typeOf _ = return SS
+instance Typed BoolExpr where typeOf _ = return BB
+instance Typed RatExpr  where typeOf _ = return QQ
+instance Typed Doc      where typeOf _ = return DD
+
+instance Typed Expr where
+  typeOf (StrE   x) = typeOf x
+  typeOf (IntE   x) = typeOf x
+  typeOf (RatE   x) = typeOf x
+  typeOf (BoolE  x) = typeOf x
+  typeOf (ListE  x) = typeOf x
+  typeOf (MacE   x) = typeOf x
+  typeOf (DocE   x) = typeOf x
+  typeOf (MatE   x) = typeOf x
+  typeOf (PolyE  x) = typeOf x
+  typeOf (PermE  x) = typeOf x
+  typeOf (ZZModE x) = typeOf x
+
+
+{-------------}
+{- :ListExpr -}
+{-------------}
+
+instance Typed ListExpr where
+  typeOf (ListVar key :@ loc) = do
+    expr <- lookupKey loc key
+    t <- typeOf expr
+    case t of
+      ListOf x -> return $ ListOf x
+      u -> reportErr loc $ ListExpected u
+
+  typeOf (ListMacro _ x :@ _) = typeOf x
+
+  typeOf (ListConst t _ :@ _) = return $ ListOf t
+
+  typeOf (ListIfThenElse _ t f :@ loc) = do
+    a <- typeOf t
+    b <- typeOf f
+    case unify a b of
+      Right u -> return u
+      Left err -> reportErr loc err
+
+  typeOf (ListAtPos x _ :@ loc) = do
+    t <- typeOf x
+    case t of
+      MatOf (ListOf u) -> return $ ListOf u
+      _ -> reportErr loc $ ListListExpected t
+
+  typeOf (ListAtIdx x _ _ :@ loc) = do
+    t <- typeOf x
+    case t of
+      ListOf (ListOf u) -> return $ ListOf u
+      _ -> reportErr loc $ ListMatrixExpected t
+
+  typeOf (ListCat  a b :@ loc) = sameType loc a b
+  typeOf (ListToss a b :@ loc) = sameType loc a b
+
+  typeOf (ListShuffle a :@ _) = typeOf a
+
+  typeOf (ListRev x :@ _) = typeOf x
+  typeOf (ListSort x :@ _) = typeOf x
+  typeOf (ListUniq x :@ _) = typeOf x
+
+  typeOf (ListRange _ _ :@ _) = return $ ListOf ZZ
+
+  typeOf (ListBuilder e _ :@ _) = do
+    t <- typeOf e
+    return $ ListOf t
+
+  typeOf (ListRand xs :@ loc) = do
+    t <- typeOf xs
+    case t of
+      ListOf (ListOf u) -> return $ ListOf u
+      _ -> reportErr loc $ ListListExpected t
+
+  typeOf (ListChoose _ xs :@ _) = typeOf xs
+
+  typeOf (ListChoices _ xs :@ _) = do
+    t <- typeOf xs
+    return $ ListOf t
+
+  typeOf (ListFilter _ _ xs :@ _) = typeOf xs
+
+  typeOf (ListMatRow _ m :@ loc) = do
+    u <- expectMatrix loc m
+    return (ListOf u)
+
+  typeOf (ListMatCol _ m :@ loc) = do
+    u <- expectMatrix loc m
+    return (ListOf u)
+
+  typeOf (ListShuffles xs :@ _) = do
+    t <- typeOf xs
+    return $ ListOf t
+
+  typeOf (ListPermsOf t _ :@ _) = return (ListOf (PermOf t))
+
+
+
+{------------}
+{- :MacExpr -}
+{------------}
+
+instance Typed MacExpr where
+  typeOf (MacConst typ _ _ _ :@ _) = return $ MacTo typ
+
+  typeOf (MacVar key :@ loc) = do
+    expr <- lookupKey loc key
+    t <- typeOf expr
+    case t of
+      MacTo x -> return $ MacTo x
+      u -> reportErr loc $ MacroExpected u
+
+  typeOf (MacMacro _ expr :@ _) = do
+    t <- typeOf expr
+    return $ MacTo t
+
+  typeOf (MacAtPos ms _ :@ loc) = do
+    t <- typeOf ms
+    case t of
+      ListOf (MacTo u) -> return $ MacTo u
+      _ -> reportErr loc $ MacroListExpected t
+
+  typeOf (MacAtIdx ms _ _ :@ loc) = do
+    t <- typeOf ms
+    case t of
+      MatOf (MacTo u) -> return $ MacTo u
+      _ -> reportErr loc $ MacroMatrixExpected t
+
+  typeOf (MacRand ms :@ loc) = do
+    t <- typeOf ms
+    case t of
+      ListOf (MacTo u) -> return $ MacTo u
+      _ -> reportErr loc $ MacroListExpected t
+
+  typeOf (MacIfThenElse _ t f :@ loc) = do
+    a <- typeOf t
+    b <- typeOf f
+    case unify a b of
+      Right u -> return u
+      Left err -> reportErr loc err
+
+
+
+{------------}
+{- :MatExpr -}
+{------------}
+
+instance Typed MatExpr where
+  typeOf (MatVar key :@ loc) = do
+    expr <- lookupKey loc key
+    t <- typeOf expr
+    case t of
+      MatOf x -> return $ MatOf x
+      u -> reportErr loc $ MatrixExpected u
+
+  typeOf (MatMacro _ x :@ _) = typeOf x
+
+  typeOf (MatAtPos m _ :@ loc) = do
+    t <- typeOf m
+    case t of
+      MatOf (MatOf u) -> return $ MatOf u
+      _ -> reportErr loc $ MatrixListExpected t
+
+  typeOf (MatAtIdx m _ _ :@ loc) = do
+    t <- typeOf m
+    case t of
+      ListOf (MatOf u) -> return $ MatOf u
+      _ -> reportErr loc $ MatrixMatrixExpected t
+
+  typeOf (MatIfThenElse _ t f :@ loc) = do
+    a <- typeOf t
+    b <- typeOf f
+    case unify a b of
+      Right u -> return u
+      Left err -> reportErr loc err
+
+  typeOf (MatConst t _ :@ _) = return $ MatOf t
+
+  typeOf (MatId t _ :@ _) = return $ MatOf t
+  typeOf (MatSwapE t _ _ _ :@ _) = return $ MatOf t
+  typeOf (MatScaleE t _ _ _ :@ _) = return $ MatOf t
+  typeOf (MatAddE t _ _ _ _ :@ _) = return $ MatOf t
+
+  typeOf (MatHCat a b :@ loc) = sameType loc a b
+  typeOf (MatVCat a b :@ loc) = sameType loc a b
+  typeOf (MatAdd  a b :@ loc) = sameType loc a b
+  typeOf (MatMul  a b :@ loc) = sameType loc a b
+  typeOf (MatPow m _ :@ _) = typeOf m
+  typeOf (MatTrans m :@ _) = typeOf m
+  typeOf (MatNeg m :@ _) = typeOf m
+
+  typeOf (MatSwapRows m _ _ :@ _) = typeOf m
+  typeOf (MatSwapCols m _ _ :@ _) = typeOf m
+  typeOf (MatScaleRow m _ _ :@ _) = typeOf m
+  typeOf (MatScaleCol m _ _ :@ _) = typeOf m
+  typeOf (MatAddRow m _ _ _ :@ _) = typeOf m
+  typeOf (MatAddCol m _ _ _ :@ _) = typeOf m
+  typeOf (MatDelRow m _ :@ _) = typeOf m
+  typeOf (MatDelCol m _ :@ _) = typeOf m
+
+  typeOf (MatShuffleRows m :@ _) = typeOf m
+  typeOf (MatShuffleCols m :@ _) = typeOf m
+
+  typeOf (MatGJForm   m :@ _) = typeOf m
+  typeOf (MatGJFactor m :@ _) = typeOf m
+
+  typeOf (MatGetRow _ m :@ _) = typeOf m
+  typeOf (MatGetCol _ m :@ _) = typeOf m
+
+  typeOf (MatRowFromList t _ :@ _) = return (MatOf t)
+  typeOf (MatColFromList t _ :@ _) = return (MatOf t)
+
+  typeOf (MatRand ms :@ loc) = do
+    t <- typeOf ms
+    case t of
+      ListOf (MatOf u) -> return $ MatOf u
+      _ -> reportErr loc $ MatrixListExpected t
+
+  typeOf (MatBuilder typ _ _ _ _ _ :@ _) = return (MatOf typ)
+
+
+
+{-------------}
+{- :PolyExpr -}
+{-------------}
+
+instance Typed PolyExpr where
+  typeOf (PolyVar key :@ loc) = do
+    expr <- lookupKey loc key
+    t <- typeOf expr
+    case t of
+      PolyOver x -> return $ PolyOver x
+      u -> reportErr loc $ PolynomialExpected u
+
+  typeOf (PolyMacro _ x :@ _) = typeOf x
+
+  typeOf (PolyConst t _ :@ _) = return $ PolyOver t
+
+  typeOf (PolyAdd  a b :@ loc) = sameType loc a b
+  typeOf (PolySub  a b :@ loc) = sameType loc a b
+  typeOf (PolyMul  a b :@ loc) = sameType loc a b
+  typeOf (PolyNeg  a :@ _) = typeOf a
+  typeOf (PolyPow  a _ :@ _) = typeOf a
+
+  typeOf (PolyAtPos m _ :@ loc) = do
+    t <- typeOf m
+    case t of
+      MatOf (PolyOver u) -> return $ PolyOver u
+      _ -> reportErr loc $ PolynomialListExpected t
+
+  typeOf (PolyAtIdx m _ _ :@ loc) = do
+    t <- typeOf m
+    case t of
+      ListOf (PolyOver u) -> return $ PolyOver u
+      _ -> reportErr loc $ PolynomialMatrixExpected t
+
+  typeOf (PolyIfThenElse _ t f :@ loc) = do
+    a <- typeOf t
+    b <- typeOf f
+    case unify a b of
+      Right u -> return u
+      Left err -> reportErr loc err
+
+  typeOf (PolyRand xs :@ loc) = do
+    t <- typeOf xs
+    case t of
+      ListOf (PolyOver u) -> return $ PolyOver u
+      _ -> reportErr loc $ PolynomialListExpected t
+
+  typeOf (PolyFromRoots _ cs :@ loc) = do
+    t <- typeOf cs
+    case t of
+      ListOf u -> return (PolyOver u)
+      _ -> reportErr loc $ ListExpected t
+
+  typeOf (PolyEvalPoly p _ :@ _) = typeOf p
+
+
+
+{-------------}
+{- :PermExpr -}
+{-------------}
+
+instance Typed PermExpr where
+  typeOf (PermVar key :@ loc) = do
+    expr <- lookupKey loc key
+    t <- typeOf expr
+    case t of
+      PermOf x -> return $ PermOf x
+      u -> reportErr loc $ PermutationExpected u
+
+  typeOf (PermMacro _ x :@ _) = typeOf x
+
+  typeOf (PermConst t _ :@ _) = return $ PermOf t
+
+  typeOf (PermAtPos m _ :@ loc) = do
+    t <- typeOf m
+    case t of
+      MatOf (PermOf u) -> return $ PermOf u
+      _ -> reportErr loc $ PermutationListExpected t
+
+  typeOf (PermAtIdx m _ _ :@ loc) = do
+    t <- typeOf m
+    case t of
+      ListOf (PermOf u) -> return $ PermOf u
+      _ -> reportErr loc $ PermutationMatrixExpected t
+
+  typeOf (PermIfThenElse _ t f :@ loc) = do
+    a <- typeOf t
+    b <- typeOf f
+    case unify a b of
+      Right u -> return u
+      Left err -> reportErr loc err
+
+  typeOf (PermRand xs :@ loc) = do
+    t <- typeOf xs
+    case t of
+      ListOf (PermOf u) -> return $ PermOf u
+      _ -> reportErr loc $ PermutationListExpected t
+
+  typeOf (PermCompose p q :@ loc) = sameType loc p q
+
+  typeOf (PermInvert p :@ _) = typeOf p
+
+
+
+{--------------}
+{- :ZZModExpr -}
+{--------------}
+
+instance Typed ZZModExpr where
+  typeOf (ZZModConst (ZZModulo _ n) :@ _) = return (ZZMod n)
+
+  typeOf (ZZModVar key :@ loc) = do
+    expr <- lookupKey loc key
+    t <- typeOf expr
+    case t of
+      ZZMod n -> return $ ZZMod n
+      u -> reportErr loc $ ModularIntegerExpected u
+
+  typeOf (ZZModAtPos m _ :@ loc) = do
+    t <- typeOf m
+    case t of
+      MatOf (ZZMod n) -> return $ ZZMod n
+      _ -> reportErr loc $ ModularIntegerListExpected t
+
+  typeOf (ZZModAtIdx m _ _ :@ loc) = do
+    t <- typeOf m
+    case t of
+      ListOf (ZZMod n) -> return $ ZZMod n
+      _ -> reportErr loc $ ModularIntegerMatrixExpected t
+
+  typeOf (ZZModMacro _ x :@ _) = typeOf x
+
+  typeOf (ZZModIfThenElse _ t f :@ loc) = do
+    a <- typeOf t
+    b <- typeOf f
+    case unify a b of
+      Right u -> return u
+      Left err -> reportErr loc err
+
+  typeOf (ZZModCast n _ :@ _) = return (ZZMod n)
+
+  typeOf (ZZModNeg a :@ _) = typeOf a
+  typeOf (ZZModInv a :@ _) = typeOf a
+
+  typeOf (ZZModAdd  a b :@ loc) = sameType loc a b
+  typeOf (ZZModSub  a b :@ loc) = sameType loc a b
+  typeOf (ZZModMult a b :@ loc) = sameType loc a b
+  typeOf (ZZModPow  a _ :@ _)   = typeOf a
+
+  typeOf (ZZModSum xs :@ loc) = do
+    t <- typeOf xs
+    case t of
+      ListOf u -> return u
+      u -> reportErr loc $ ListExpected u
+
+  typeOf (ZZModProd xs :@ loc) = do
+    t <- typeOf xs
+    case t of
+      ListOf u -> return u
+      u -> reportErr loc $ ListExpected u
