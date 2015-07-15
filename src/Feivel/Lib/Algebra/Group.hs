3fa96918cd2de8fb83a48f49a8014fb6d5da022c
nathan <nbloomf@gmail.com>
2015-07-15 12:58:45 -0500

    Created Lib.Algebra.Group

---
+++ src/Feivel/Lib/Algebra/Group.hs
@@ -0,0 +1,30 @@
+{---------------------------------------------------------------------}
+{- Copyright 2015 Nathan Bloomfield                                  -}
+{-                                                                   -}
+{- This file is part of Feivel.                                      -}
+{-                                                                   -}
+{- Feivel is free software: you can redistribute it and/or modify    -}
+{- it under the terms of the GNU General Public License version 3,   -}
+{- as published by the Free Software Foundation.                     -}
+{-                                                                   -}
+{- Feivel is distributed in the hope that it will be useful, but     -}
+{- WITHOUT ANY WARRANTY; without even the implied warranty of        -}
+{- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the      -}
+{- GNU General Public License for more details.                      -}
+{-                                                                   -}
+{- You should have received a copy of the GNU General Public License -}
+{- along with Feivel. If not, see <http://www.gnu.org/licenses/>.    -}
+{---------------------------------------------------------------------}
+
+module Feivel.Lib.Algebra.Group (
+) where
+
+import Feivel.Lib.AlgErr
+
+class Groupoid t where
+  gOp :: t -> t -> Either AlgErr t
+
+  gId :: t
+  gIsId :: t -> Bool
+
+  gIdOf :: t -> Either AlgErr t
