79d41753a32ac0d1854f72bba3b44cc2efed9e25
nathan <nbloomf@gmail.com>
2015-07-18 16:44:23 -0500

    Moved Matrix and polynomial eval instances to separate modules

---
+++ src/Feivel/Eval/Mat.hs
@@ -0,0 +1,328 @@
+{---------------------------------------------------------------------}
+{- Copyright 2015 Nathan Bloomfield                                  -}
+{-                                                                   -}
+{- This file is part of Feivel.                                      -}
+{-                                                                   -}
+{- Feivel is free software: you can redistribute it and/or modify    -}
+{- it under the terms of the GNU General Public License version 3,   -}
+{- as published by the Free Software Foundation.                     -}
+{-                                                                   -}
+{- Feivel is distributed in the hope that it will be useful, but     -}
+{- WITHOUT ANY WARRANTY; without even the implied warranty of        -}
+{- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the      -}
+{- GNU General Public License for more details.                      -}
+{-                                                                   -}
+{- You should have received a copy of the GNU General Public License -}
+{- along with Feivel. If not, see <http://www.gnu.org/licenses/>.    -}
+{---------------------------------------------------------------------}
+
+{-# LANGUAGE TypeSynonymInstances  #-}
+{-# LANGUAGE FlexibleInstances     #-}
+{-# LANGUAGE FlexibleContexts      #-}
+
+module Feivel.Eval.Mat where
+
+import Feivel.EvalM
+import Feivel.Eval.Eval
+import Feivel.Eval.Util
+import Feivel.Expr
+import Feivel.Type
+import Feivel.Key
+import Feivel.Get
+import Feivel.Put
+import Feivel.Typed
+import Feivel.Lib
+import Feivel.Locus
+import Feivel.Error
+
+{-----------------}
+{- :Eval:MatExpr -}
+{-----------------}
+
+instance (Eval Expr) => Eval (MatExpr Expr) where
+  eval (MatConst t m :@ loc) = do
+    n <- mSeq $ fmap eval m
+    return $ MatConst t n :@ loc
+
+  {- :Common -}
+  eval (MatVar _ key :@ loc)        = eKey key loc
+  eval (MatAtIdx _ m h k :@ loc)    = eAtIdx m h k loc
+  eval (MatMacro _ vals mac :@ loc) = eMacro vals mac loc
+  eval (MatIfThenElse _ b t f :@ _) = eIfThenElse b t f
+
+  eval (MatAtPos _ a t :@ loc) = lift2 loc a t (foo)
+    where foo = listAtPos :: [MatExpr Expr] -> Integer -> Either ListErr (MatExpr Expr)
+
+  eval (MatRand _ ls :@ _) = do
+    xs <- eval ls >>= getVal :: EvalM [Expr]
+    r  <- randomElementEvalM xs
+    eval r >>= getVal
+
+  eval (MatRowFromList t xs :@ loc) = do
+    as <- eval xs >>= getVal :: EvalM [Expr]
+    m  <- tryEvalM loc $ mRowFromList as
+    return (MatConst t m :@ loc)
+
+  eval (MatColFromList t xs :@ loc) = do
+    as <- eval xs >>= getVal :: EvalM [Expr]
+    m  <- tryEvalM loc $ mColFromList as
+    return (MatConst t m :@ loc)
+
+  eval (MatId u n :@ loc) = do
+    let makeIdMat x = lift1 loc n (mEIdT x)
+    case u of
+      ZZ          -> makeIdMat zeroZZ
+      QQ          -> makeIdMat zeroQQ
+      BB          -> makeIdMat zeroBB
+      ZZMod k     -> makeIdMat (zeroMod k)
+      PolyOver ZZ -> makeIdMat (constP zeroZZ)
+      PolyOver QQ -> makeIdMat (constP zeroQQ)
+      _           -> reportErr loc $ NumericTypeExpected u
+
+  eval (MatSwapE u n h k :@ loc) = do
+    let makeSwapMat x = lift3 loc n h k (mESwapT x)
+    case u of
+      ZZ          -> makeSwapMat zeroZZ
+      QQ          -> makeSwapMat zeroQQ
+      BB          -> makeSwapMat zeroBB
+      ZZMod n     -> makeSwapMat (zeroMod n)
+      PolyOver ZZ -> makeSwapMat (constP zeroZZ)
+      PolyOver QQ -> makeSwapMat (constP zeroQQ)
+      _           -> reportErr loc $ NumericTypeExpected u
+
+  eval (MatScaleE u n h r :@ loc) = do
+    let makeScaleMat x = lift3 loc n h r (mEScaleT x)
+    case u of
+      ZZ          -> makeScaleMat zeroZZ
+      QQ          -> makeScaleMat zeroQQ
+      BB          -> makeScaleMat zeroBB
+      ZZMod n     -> makeScaleMat (zeroMod n)
+      PolyOver ZZ -> makeScaleMat (constP zeroZZ)
+      PolyOver QQ -> makeScaleMat (constP zeroQQ)
+      PolyOver BB -> makeScaleMat (constP zeroBB)
+      _           -> reportErr loc $ NumericTypeExpected u
+
+  eval (MatAddE u n h k r :@ loc) = do
+    let makeAddMat x = lift4 loc n h k r (mEAddT x)
+    case u of
+      ZZ          -> makeAddMat zeroZZ
+      QQ          -> makeAddMat zeroQQ
+      BB          -> makeAddMat zeroBB
+      ZZMod d     -> makeAddMat (zeroMod d)
+      PolyOver ZZ -> makeAddMat (constP zeroZZ)
+      PolyOver QQ -> makeAddMat (constP zeroQQ)
+      PolyOver BB -> makeAddMat (constP zeroBB)
+      _           -> reportErr loc $ NumericTypeExpected u
+
+  eval (MatShuffleRows u m :@ loc) = do
+    x  <- eval m >>= getVal :: EvalM (Matrix Expr)
+    rs <- tryEvalM loc $ mRowsOf x
+    ts <- shuffleEvalM rs
+    n  <- tryEvalM loc $ mVCats ts
+    return (MatConst u n :@ loc)
+
+  eval (MatShuffleCols u m :@ loc) = do
+    x  <- eval m >>= getVal :: EvalM (Matrix Expr)
+    rs <- tryEvalM loc $ mColsOf x
+    ts <- shuffleEvalM rs
+    n  <- tryEvalM loc $ mHCats ts
+    return (MatConst u n :@ loc)
+
+  eval (MatHCat u a b :@ loc) = do
+    m <- eval a >>= getVal :: EvalM (Matrix Expr)
+    n <- eval b >>= getVal :: EvalM (Matrix Expr)
+    x <- tryEvalM loc $ mHCat m n
+    return (MatConst u x :@ loc)
+
+  eval (MatVCat u a b :@ loc) = do
+    m <- eval a >>= getVal :: EvalM (Matrix Expr)
+    n <- eval b >>= getVal :: EvalM (Matrix Expr)
+    x <- tryEvalM loc $ mVCat m n
+    return $ MatConst u x :@ loc
+
+  eval (MatAdd u a b :@ loc) = do
+    let addMat x = lift2 loc a b (rAddT (mCell x))
+    case u of
+      ZZ          -> addMat zeroZZ
+      QQ          -> addMat zeroQQ
+      BB          -> addMat zeroBB
+      ZZMod n     -> addMat (zeroMod n)
+      PolyOver ZZ -> addMat (constP zeroZZ)
+      PolyOver QQ -> addMat (constP zeroQQ)
+      PolyOver BB -> addMat (constP zeroBB)
+      _           -> reportErr loc $ NumericTypeExpected u
+
+  eval (MatNeg u a :@ loc) = do
+    let negMat x = lift1 loc a (rNegT (mCell x))
+    case u of
+      ZZ          -> negMat zeroZZ
+      QQ          -> negMat zeroQQ
+      BB          -> negMat zeroBB
+      ZZMod n     -> negMat (zeroMod n)
+      PolyOver ZZ -> negMat (constP zeroZZ)
+      PolyOver QQ -> negMat (constP zeroQQ)
+      PolyOver BB -> negMat (constP zeroBB)
+      _           -> reportErr loc $ NumericTypeExpected u
+
+  eval (MatTrans _ a :@ loc) = do
+    u <- expectMatrix loc a
+    m <- eval a >>= getVal :: EvalM (Matrix Expr)
+    p <- tryEvalM loc $ mTranspose m
+    return (MatConst u p :@ loc)
+
+  eval (MatMul u a b :@ loc) = do
+    let mulMat x = lift2 loc a b (rMulT (mCell x))
+    case u of
+      ZZ          -> mulMat zeroZZ
+      QQ          -> mulMat zeroQQ
+      BB          -> mulMat zeroBB
+      (ZZMod n)   -> mulMat (zeroMod n)
+      PolyOver ZZ -> mulMat (constP zeroZZ)
+      PolyOver QQ -> mulMat (constP zeroQQ)
+      PolyOver BB -> mulMat (constP zeroBB)
+      _           -> reportErr loc $ NumericTypeExpected u
+
+  eval (MatPow u m n :@ loc) = do
+    let powMat x = lift2 loc m n (rPosPowT (mCell x))
+    case u of
+      ZZ          -> powMat zeroZZ
+      QQ          -> powMat zeroQQ
+      BB          -> powMat zeroBB
+      (ZZMod k)   -> powMat (zeroMod k)
+      PolyOver ZZ -> powMat (constP zeroZZ)
+      PolyOver QQ -> powMat (constP zeroQQ)
+      PolyOver BB -> powMat (constP zeroBB)
+      _           -> reportErr loc $ NumericTypeExpected u
+
+  eval (MatSwapRows _ m a b :@ loc) = do
+    u <- expectMatrix loc m
+    n <- eval m >>= getVal :: EvalM (Matrix Expr)
+    i <- eval a >>= getVal
+    j <- eval b >>= getVal
+    p <- tryEvalM loc $ mSwapRows i j n
+    return $ MatConst u p :@ loc
+
+  eval (MatSwapCols _ m a b :@ loc) = do
+    u <- expectMatrix loc m
+    n <- eval m >>= getVal :: EvalM (Matrix Expr)
+    i <- eval a >>= getVal
+    j <- eval b >>= getVal
+    p <- tryEvalM loc $ mSwapCols i j n
+    return $ MatConst u p :@ loc
+
+  eval (MatScaleRow u m a h :@ loc) = do
+    let scaleRowMat x = lift3 loc a h m (mScaleRowT x)
+    case u of
+      ZZ          -> scaleRowMat zeroZZ
+      QQ          -> scaleRowMat zeroQQ
+      BB          -> scaleRowMat zeroBB
+      (ZZMod n)   -> scaleRowMat (zeroMod n)
+      PolyOver ZZ -> scaleRowMat (constP zeroZZ)
+      PolyOver QQ -> scaleRowMat (constP zeroQQ)
+      PolyOver BB -> scaleRowMat (constP zeroBB)
+      _           -> reportErr loc $ NumericTypeExpected u
+
+  eval (MatScaleCol u m a h :@ loc) = do
+    let scaleColMat x = lift3 loc a h m (mScaleColT x)
+    case u of
+      ZZ          -> scaleColMat zeroZZ
+      QQ          -> scaleColMat zeroQQ
+      BB          -> scaleColMat zeroBB
+      (ZZMod n)   -> scaleColMat (zeroMod n)
+      PolyOver ZZ -> scaleColMat (constP zeroZZ)
+      PolyOver QQ -> scaleColMat (constP zeroQQ)
+      PolyOver BB -> scaleColMat (constP zeroBB)
+      _           -> reportErr loc $ NumericTypeExpected u
+
+  eval (MatAddRow _ m a h k :@ loc) = do
+    let t = typeOf m
+    let u = typeOf a
+    i <- eval h >>= getVal
+    j <- eval k >>= getVal
+    let makeAddRow zer = do
+          n <- eval m >>= getVal
+          r <- eval a >>= getVal
+          suchThat $ r `hasSameTypeAs` zer
+          p <- tryEvalM loc $ mAddRow r i j n
+          getVal (put loc p)
+    case unify t (MatOf u) of
+      Right (MatOf ZZ) -> makeAddRow zeroZZ
+      Right (MatOf QQ) -> makeAddRow zeroQQ
+      Right (MatOf BB) -> makeAddRow zeroBB
+      Right (MatOf (ZZMod n)) -> makeAddRow (zeroMod n)
+      Right w  -> reportErr loc $ NumericMatrixExpected w
+      Left err -> reportErr loc err
+
+  eval (MatAddCol _ m a h k :@ loc) = do
+    let t = typeOf m
+    let u = typeOf a
+    i <- eval h >>= getVal
+    j <- eval k >>= getVal
+    let makeAddCol zer = do
+          n <- eval m >>= getVal
+          r <- eval a >>= getVal
+          suchThat $ r `hasSameTypeAs` zer
+          p <- tryEvalM loc $ mAddCol r i j n
+          getVal (put loc p)
+    case unify t (MatOf u) of
+      Right (MatOf ZZ) -> makeAddCol zeroZZ
+      Right (MatOf QQ) -> makeAddCol zeroQQ
+      Right (MatOf BB) -> makeAddCol zeroBB
+      Right (MatOf (ZZMod n)) -> makeAddCol (zeroMod n)
+      Right w -> reportErr loc $ NumericMatrixExpected w
+      Left err -> reportErr loc err
+
+  eval (MatDelRow _ m a :@ loc) = do
+    u <- expectMatrix loc m
+    n <- eval m >>= getVal :: EvalM (Matrix Expr)
+    i <- eval a >>= getVal
+    p <- tryEvalM loc $ mDelRow n i
+    return $ MatConst u p :@ loc
+
+  eval (MatDelCol _ m a :@ loc) = do
+    u <- expectMatrix loc m
+    n <- eval m >>= getVal :: EvalM (Matrix Expr)
+    i <- eval a >>= getVal
+    p <- tryEvalM loc $ mDelCol n i
+    return $ MatConst u p :@ loc
+
+  eval (MatGJForm u m :@ loc) = do
+    let gjFormMat x = lift1 loc m (mGJFormT x)
+    case u of
+      QQ -> gjFormMat zeroQQ
+      BB -> gjFormMat zeroBB
+      _  -> reportErr loc $ FieldTypeExpected u
+
+  eval (MatGJFactor u m :@ loc) = do
+    let gjFactorMat x = lift1 loc m (mGJFactorT x)
+    case u of
+      QQ -> gjFactorMat zeroQQ
+      BB -> gjFactorMat zeroBB
+      _  -> reportErr loc $ FieldTypeExpected u
+
+  eval (MatGetRow _ k m :@ loc) = do
+    u <- expectMatrix loc m
+    i <- eval k >>= getVal :: EvalM Integer
+    n <- eval m >>= getVal :: EvalM (Matrix Expr)
+    r <- tryEvalM loc $ mRowOf i n
+    return (MatConst u r :@ loc)
+
+  eval (MatGetCol _ k m :@ loc) = do
+    u <- expectMatrix loc m
+    i <- eval k >>= getVal :: EvalM Integer
+    n <- eval m >>= getVal :: EvalM (Matrix Expr)
+    c <- tryEvalM loc $ mColOf i n
+    return (MatConst u c :@ loc)
+
+  eval (MatBuilder typ e kr lr kc lc :@ loc) = do
+    st <- getState
+    rs <- eval lr >>= getVal :: EvalM [Expr]
+    cs <- eval lc >>= getVal :: EvalM [Expr]
+    es <- sequence [sequence [foo st r c | c <- cs] | r <- rs]
+    m  <- tryEvalM loc $ mFromRowList es
+    return (MatConst typ m :@ loc)
+      where
+        foo st r c = do
+          st' <- addKeyToStore kr r loc st >>= addKeyToStore kc c loc
+          evalWith e st' >>= getVal
+
---
+++ src/Feivel/Eval/Poly.hs
@@ -0,0 +1,179 @@
+{---------------------------------------------------------------------}
+{- Copyright 2015 Nathan Bloomfield                                  -}
+{-                                                                   -}
+{- This file is part of Feivel.                                      -}
+{-                                                                   -}
+{- Feivel is free software: you can redistribute it and/or modify    -}
+{- it under the terms of the GNU General Public License version 3,   -}
+{- as published by the Free Software Foundation.                     -}
+{-                                                                   -}
+{- Feivel is distributed in the hope that it will be useful, but     -}
+{- WITHOUT ANY WARRANTY; without even the implied warranty of        -}
+{- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the      -}
+{- GNU General Public License for more details.                      -}
+{-                                                                   -}
+{- You should have received a copy of the GNU General Public License -}
+{- along with Feivel. If not, see <http://www.gnu.org/licenses/>.    -}
+{---------------------------------------------------------------------}
+
+{-# LANGAUGE TypeSynonymInstances #-}
+{-# LANGUAGE FlexibleInstances    #-}
+{-# LANGUAGE FlexibleContexts     #-}
+
+module Feivel.Eval.Poly where
+
+import Feivel.EvalM
+import Feivel.Expr
+import Feivel.Type
+import Feivel.Locus
+import Feivel.Lib
+import Feivel.Get
+import Feivel.Put
+import Feivel.Typed
+import Feivel.Eval.Eval
+import Feivel.Eval.Util
+import Feivel.Error
+
+{------------------}
+{- :Eval:PolyExpr -}
+{------------------}
+
+instance (Eval Expr) => Eval (PolyExpr Expr) where
+  eval (PolyConst t p :@ loc) = do
+    q <- polySeq $ fmap eval p
+    return $ PolyConst t q :@ loc
+
+  {- :Common -}
+  eval (PolyVar _ key :@ loc)        = eKey key loc
+  eval (PolyAtIdx _ m h k :@ loc)    = eAtIdx m h k loc
+  eval (PolyMacro _ vals mac :@ loc) = eMacro vals mac loc
+  eval (PolyIfThenElse _ b t f :@ _) = eIfThenElse b t f
+
+  eval (PolyAtPos _ a t :@ loc) = lift2 loc a t (foo)
+    where foo = listAtPos :: [PolyExpr Expr] -> Integer -> Either ListErr (PolyExpr Expr)
+
+  eval (PolyRand _ ls :@ loc) = do
+    let t = typeOf ls
+    xs <- eval ls >>= getVal :: EvalM [Expr]
+    r  <- randomElementEvalM xs
+    s  <- eval r >>= getVal :: EvalM (PolyExpr Expr)
+    case t of
+      ListOf (PolyOver _) -> return s
+      _ -> reportErr loc $ ListExpected t
+
+  eval (PolyAdd u a b :@ loc) = do
+    let addPoly x = lift2 loc a b (rAddT (constP x))
+    case u of
+      ZZ          -> addPoly zeroZZ
+      QQ          -> addPoly zeroQQ
+      BB          -> addPoly zeroBB
+      (ZZMod n)   -> addPoly (zeroMod n)
+      PolyOver ZZ -> addPoly (constP zeroZZ)
+      PolyOver QQ -> addPoly (constP zeroQQ)
+      PolyOver BB -> addPoly (constP zeroBB)
+      _           -> reportErr loc $ NumericTypeExpected u
+
+  eval (PolySub u a b :@ loc) = do
+    let subPoly x = lift2 loc a b (rSubT (constP x))
+    case u of
+      ZZ          -> subPoly zeroZZ
+      QQ          -> subPoly zeroQQ
+      BB          -> subPoly zeroBB
+      (ZZMod n)   -> subPoly (zeroMod n)
+      PolyOver ZZ -> subPoly (constP zeroZZ)
+      PolyOver QQ -> subPoly (constP zeroQQ)
+      PolyOver BB -> subPoly (constP zeroBB)
+      _           -> reportErr loc $ NumericTypeExpected u
+
+  eval (PolyMul u a b :@ loc) = do
+    let mulPoly x = lift2 loc a b (rMulT (constP x))
+    case u of
+      ZZ          -> mulPoly zeroZZ
+      QQ          -> mulPoly zeroQQ
+      BB          -> mulPoly zeroBB
+      (ZZMod n)   -> mulPoly (zeroMod n)
+      PolyOver ZZ -> mulPoly (constP zeroZZ)
+      PolyOver QQ -> mulPoly (constP zeroQQ)
+      PolyOver BB -> mulPoly (constP zeroBB)
+      _           -> reportErr loc $ NumericTypeExpected u
+
+  eval (PolyNeg _ a :@ loc) = do
+    let t = typeOf a
+    case t of
+      PolyOver ZZ ->
+        lift1 loc a (rNegT (constP (0::Integer)))
+      PolyOver QQ ->
+        lift1 loc a (rNegT (constP (0:/:1)))
+      PolyOver BB ->
+        lift1 loc a (rNegT (constP False))
+      PolyOver (ZZMod n) -> do
+        x <- eval a >>= getVal :: EvalM (Poly ZZModulo)
+        let y = rNeg x
+        return $ PolyConst (ZZMod n) (fmap toExpr y) :@ loc
+      _ -> reportErr loc $ NumericPolynomialExpected t
+
+  eval (PolyPow _ a b :@ loc) = do
+    let t = typeOf a
+    case t of
+      PolyOver ZZ ->
+        lift2 loc a b (rPowT (constP (0::Integer)))
+      PolyOver QQ ->
+        lift2 loc a b (rPowT (constP (0:/:1)))
+      PolyOver BB ->
+        lift2 loc a b (rPowT (constP False))
+      PolyOver (ZZMod n) -> do
+        x <- eval a >>= getVal :: EvalM (Poly ZZModulo)
+        y <- eval b >>= getVal :: EvalM Integer
+        z <- tryEvalM loc $ rPow x y
+        return $ PolyConst (ZZMod n) (fmap toExpr z) :@ loc
+      _ -> reportErr loc $ NumericPolynomialExpected t
+
+  eval (PolyFromRoots _ x cs :@ loc) = do
+    let t = typeOf cs
+    case t of
+      ListOf ZZ -> do
+        as <- eval cs >>= getVal :: EvalM [Integer]
+        p  <- tryEvalM loc $ fromRootsP x as
+        let q = fmap toExpr p
+        return (PolyConst ZZ q :@ loc)
+      ListOf QQ -> do
+        as <- eval cs >>= getVal :: EvalM [Rat]
+        p  <- tryEvalM loc $ fromRootsP x as
+        let q = fmap toExpr p
+        return (PolyConst QQ q :@ loc)
+      ListOf (ZZMod n) -> do
+        as <- eval cs >>= getVal :: EvalM [ZZModulo]
+        p  <- tryEvalM loc $ fromRootsP x as
+        let q = fmap toExpr p
+        return (PolyConst (ZZMod n) q :@ loc)
+      _ -> reportErr loc $ NumericListExpected t
+
+  eval (PolyEvalPoly _ p qs :@ loc) = do
+    let t = typeOf p
+    case t of
+      PolyOver ZZ -> do
+        a <- eval p >>= getVal :: EvalM (Poly Integer)
+        let foo (x,h) = do
+              b <- eval h >>= getVal :: EvalM (Poly Integer)
+              return (x,b)
+        ks <- sequence $ map foo qs
+        c  <- tryEvalM loc $ evalPolyAtPolysP ks a
+        return (PolyConst ZZ (fmap toExpr c) :@ loc)
+      PolyOver QQ -> do
+        a <- eval p >>= getVal :: EvalM (Poly Rat)
+        let foo (x,h) = do
+              b <- eval h >>= getVal :: EvalM (Poly Rat)
+              return (x,b)
+        ks <- sequence $ map foo qs
+        c  <- tryEvalM loc $ evalPolyAtPolysP ks a
+        return (PolyConst QQ (fmap toExpr c) :@ loc)
+      PolyOver (ZZMod n) -> do
+        a <- eval p >>= getVal :: EvalM (Poly ZZModulo)
+        let foo (x,h) = do
+              b <- eval h >>= getVal :: EvalM (Poly ZZModulo)
+              return (x,b)
+        ks <- sequence $ map foo qs
+        c  <- tryEvalM loc $ evalPolyAtPolysP ks a
+        return (PolyConst (ZZMod n) (fmap toExpr c) :@ loc)
+      _ -> reportErr loc $ NumericPolynomialExpected t
+
